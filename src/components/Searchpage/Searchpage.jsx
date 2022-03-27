b247c9d918e7704a4d3eae7279d7746fe00bcbfc
karthiksb <34330740+karthiksb@users.noreply.github.com>
2022-03-27 14:29:23 +0530

    added search page

--- src/components/Searchpage/Searchpage.jsx
+++ src/components/Searchpage/Searchpage.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import "./searchpage.css";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import MicRoundedIcon from "@mui/icons-material/MicRounded";
 
 function searchpage() {
-  return <div className="searchpage"></div>;
+  return (
+    <div className="searchpage">
+      <div className="header">
+        <div className="links">
+          <ul>
+            <p>Gmail</p>
+            <p>Images</p>
+            <p>All</p>
+
+            <p>Account</p>
+          </ul>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="google-search">
+          <h1>Google</h1>
+          <div className="searchdiv">
+            <div className="searchbox">
+              <SearchRoundedIcon />
+              <input type="text" className="searchinput"></input>
+              <MicRoundedIcon />
+            </div>
+          </div>
+
+          <div className="buttons">
+            <button>Google Search</button>
+            <button>I'm Feeling Lucky</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default searchpage;
